3db204f4a286ae7f583427142cd5e4d09efa68f6
BhaveshKhaple <bhaveshkhaple@gmail.com>
2026-02-28 11:10:20 +0530

    fix(badge): remove orphan Badge exports, fix button import path to mohammad

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,6 +1,3 @@
-
-export { Badge, badgeVariants }
-
 "use client"
 
 import * as React from "react"
@@ -8,7 +5,7 @@ import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
-import { buttonVariants } from "@/components/ui/button"
+import { buttonVariants } from "@/components/ui/mohammad"
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
