95bd63b10a98285f94d14a236a3cb37da9c1b162
BhaveshKhaple <bhaveshkhaple@gmail.com>
2026-02-28 11:10:39 +0530

    fix(chart): remove orphan ChartContainer exports (Card file, not Chart)

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -1,14 +1,3 @@
-
-
-export {
-  ChartContainer,
-  ChartTooltip,
-  ChartTooltipContent,
-  ChartLegend,
-  ChartLegendContent,
-  ChartStyle,
-}
-
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
